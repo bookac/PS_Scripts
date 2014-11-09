e21f62ea7b983e465c84e2f7abab0a534bb8bdd0
zokipokidiablo <panev.zoran.te@gmail.com>
2014-11-09 02:07:22 +0100

    Game Over option

--- Game architectura/PlayerAttackControl.cs
+++ Game architectura/PlayerAttackControl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerAttackControl : MonoBehaviour {
@@ -6,21 +7,58 @@ public class PlayerAttackControl : MonoBehaviour {
 	public RandomizeEnemy enemy;
 	public GameObject EnemyToDie;
 
+	List<GameObject> listOfDedlineEnemys;
+	bool gameOver;
+
+	void Start(){
+		listOfDedlineEnemys = new List<GameObject> ();
+		gameOver = false;
+	}
+
 	void OnTriggerEnter(Collider collision){
 		// If occurre collision with some enemy object do this
 		if(collision.gameObject == EnemyToDie){
 			PlayerAttack(EnemyToDie);
 		}
+
+		for (int i=0; i<enemy.listOfEnemys.Count; i++) {
+			if(enemy.listOfEnemys[i] == collision.gameObject){
+				gameOver = true;
+				listOfDedlineEnemys.Add(collision.gameObject);
+			}
+		}
+
+		if(gameOver){
+			StartCoroutine("GameOver", 0.4f);
+		}
+	}
+
+	void OnTriggerExit(Collider collision){
+		listOfDedlineEnemys.Remove(collision.gameObject);
+		if(listOfDedlineEnemys.Count == 0){
+			gameOver = false;
+			listOfDedlineEnemys.Clear();
+		}
 	}
 
 	void PlayerAttack(GameObject obj){
 		enemy.listOfEnemys.Remove(obj);
-		enemy.listOfCollidedEnemys.Remove(obj);
-		enemy.listOfDedlineEnemys.Remove(obj);
+		listOfDedlineEnemys.Remove(obj);
 		DieEnemy(obj);
 	}
 	
 	void DieEnemy(GameObject obj){
 		Destroy(obj);
+	}
+
+	IEnumerator GameOver(float waitTime){
+		yield return new WaitForSeconds(waitTime);
+		if(gameOver){
+			if(listOfDedlineEnemys.Count != 0){
+				print("GameOver");
+				Application.LoadLevel(0);
+			}
+		}
+		StopCoroutine("GameOver");
 	}
 }
--- Game architectura/RandomizeEnemy.cs
+++ Game architectura/RandomizeEnemy.cs
@@ -6,22 +6,20 @@ public class RandomizeEnemy : MonoBehaviour {
 	#region PUBLIC OBJECTS
 	public GameObject enemy, player;
 	public int enemyCounter, enemyLimit;
-	public List<GameObject> listOfEnemys, listOfCollidedEnemys, listOfDedlineEnemys;
+	public List<GameObject> listOfEnemys, listOfCollidedEnemys;
 	#endregion
 
 	#region LOCAL PRIVATE OBJECTS
-	float xPos, zPos, timeTolerance, startCountdown;
-	bool GameOver;
+	float xPos, zPos;
 	GameObject clone;
 	#endregion
 
 	// Use this for initialization
 	void Start () {
 		clone = new GameObject ("Clone");
-		enemyLimit = 12; timeTolerance = 0.3f; GameOver = false; startCountdown = 0f;
+		enemyLimit = 12;
 		listOfEnemys = new List<GameObject> ();
 		listOfCollidedEnemys = new List<GameObject> ();
-		listOfDedlineEnemys = new List<GameObject> ();
 		StartCoroutine ("Randomize", 2f);
 	}
 
@@ -90,28 +88,6 @@ public class RandomizeEnemy : MonoBehaviour {
 			}catch{}
 		}
 	}
-
-	void OnCollisionEnter(Collision collision){
-		print("enter = " + collision.gameObject.name);
-		for (int i=0; i<listOfEnemys.Count; i++) {
-			if(listOfEnemys[i] == collision.gameObject){
-				GameOver = true; startCountdown = Time.time;
-				listOfDedlineEnemys.Add(collision.gameObject);
-			}
-		}
-	}
-
-	void OnCollisionExit(Collision collision){
-		for (int i=0; i<listOfDedlineEnemys.Count; i++) {
-			if(listOfDedlineEnemys[i] == collision.gameObject){
-				listOfDedlineEnemys.Remove(listOfDedlineEnemys[i]);
-			}
-		}
-		print("eneme count = " + listOfDedlineEnemys.Count);
-		if(listOfDedlineEnemys.Count == 0){
-			GameOver = false;			
-		}
-	}
 	#endregion
 
 	// Update is called once per frame
@@ -119,11 +95,5 @@ public class RandomizeEnemy : MonoBehaviour {
 		Checkpoints ();
 		EnemyAttackPlayer ();
 		CorrectEnemyCollision();
-
-		if(GameOver){
-			if((Time.time - startCountdown) > timeTolerance){
-				print("GameOver");
-			}
-		}
 	}
 }
