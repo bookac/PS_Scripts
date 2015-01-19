294d9c2ddec5c29e7a50dc5497feb0ab87ee30c5
bookac <bookac@gmail.com>
2015-01-19 21:48:40 +0100

    BG_Dark_Sprite name changes

--- NGUI_Buttons/ClosePopUpButton.cs
+++ NGUI_Buttons/ClosePopUpButton.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class ClosePopUpButton : MonoBehaviour {
 
 	public GameObject PopUp;
-	public GameObject BG_Dark;
+	public GameObject BG_Dark_Sprite;
 	
 	void OnClick()
 	{
 		PopUp.animation.Play ("PopUp_Close");
-		BG_Dark.SetActive (false);
+		BG_Dark_Sprite.SetActive (false);
 	}
 }
--- NGUI_Buttons/OpenPopUpButton.cs
+++ NGUI_Buttons/OpenPopUpButton.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class OpenPopUpButton : MonoBehaviour {
 
 	public GameObject PopUp;
-	public GameObject BG_Dark;
+	public GameObject BG_Dark_Sprite;
 
 	void OnClick()
 	{
 		transform.animation.Play ("Button_Pressed");
 		PopUp.animation.Play ("PopUp_Open");
-		BG_Dark.SetActive (true);
+		BG_Dark_Sprite.SetActive (true);
 		PopUp.SetActive (true);
 	}
 }
